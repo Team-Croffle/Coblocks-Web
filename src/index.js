b21e1d17dfdcbce449adffbc4781ebdd32232c83
unbaen <rlagotjd62@gmail.com>
2025-05-01 17:57:51 +0900

    renmae: change file strucutre
    -파일 구조 변경

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './containers/App';
+import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/modules/i18n/i18n'; // i18n 초기화
-import './styles/index.css';
-import App from './App';
+import App from './App'; // App 컴포넌트 가져오기
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
