f45a8cf39f3c374dba1df8ac58b7a7dc6ed22260
BlueNyang <xpsj20@gmail.com>
2025-05-02 13:40:03 +0900

    refactor: migration to Vite

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './components/modules/i18n/i18n'; // i18n 초기화
-import App from './App'; // App 컴포넌트 가져오기
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
